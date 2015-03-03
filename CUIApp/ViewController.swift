73789efc929e7b4bef0a1a1c1f09f3cb15ca9dd1
mguida22 <michael.guida@colorado.edu>
2015-03-03 15:36:49 -0700

    Added TableView

--- CUIApp/ViewController.swift
+++ CUIApp/ViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // Data to populate table with
+    let tableData = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+    
+    // Identifier to use for the text cell in the tableview. This should be updated if the tableview text
+    // cell identifier is updated.
+    let textCellIdentifier = "TextCell"
 
+    // TableView on the main screen
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        let row = indexPath.row
+        
+        // Setting the cells text to the corresponding data
+        cell.textLabel.text = tableData[row]
+        
+        return cell
     }
 
 
