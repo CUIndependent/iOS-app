0f88da5fba7fc03a127c7f6aa73b1ad349ee8b81
mguida22 <michael.guida@colorado.edu>
2015-03-04 21:29:42 -0700

    Updating article class

--- CUIApp/Article.swift
+++ CUIApp/Article.swift
@@ -11,9 +11,11 @@ import UIKit
 class Article: NSObject {
     var title: String
     var subtitle: String
+    var time: String
     
-    init(title: String, subtitle: String) {
+    init(title: String, subtitle: String, time: String) {
         self.title = title
         self.subtitle = subtitle
+        self.time = time
     }
 }
